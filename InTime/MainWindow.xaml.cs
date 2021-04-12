80060e30c51b8edd71ed69a0a31971096fb89125
Oleg Blynov <mylteks@gmail.com>
2021-04-12 17:23:22 +0300

    db_added

--- InTime/MainWindow.xaml.cs
+++ InTime/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using InTime.ServiceReference1;
 
 namespace InTime
 {
@@ -23,6 +25,9 @@ namespace InTime
         public MainWindow()
         {
             InitializeComponent();
+            Service1Client client = new Service1Client();
+            byte[] arr = client.GetFile();
+            File.WriteAllBytes(@"C:\test_track.mp3",arr);
         }
     }
 }
